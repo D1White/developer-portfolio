27a07fc194f479e99425316de2a0d7befb28dab7
Danylo Bilyi <danilo.bilyi@gmail.com>
2022-05-11 23:51:36 +0300

    feat: add google analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import 'styles/app.scss';
 
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import Footer from '@components/Footer';
+import { pageview } from '@utils/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Component {...pageProps} />
--- types/environment.d.ts
+++ types/environment.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_CONTENTFUL_SPACE_ID: string;
       NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: string;
+      NEXT_PUBLIC_GOOGLE_ANALYTICS: string;
       NODE_ENV: 'development' | 'production';
     }
   }
