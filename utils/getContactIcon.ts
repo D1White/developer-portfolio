37f3ad113575d13c198936ab754a7a1d256d2b48
Danylo Bilyi <danilo.bilyi@gmail.com>
2023-02-05 17:40:46 +0200

    feat: add lin icon, change avatar

--- utils/getContactIcon.ts
+++ utils/getContactIcon.ts
@@ -2,12 +2,14 @@ import telegramIcon from '@svg/telegram.svg';
 import githubIcon from '@svg/github.svg';
 import behanceIcon from '@svg/behance.svg';
 import habrIcon from '@svg/habr.svg';
+import linkedinIcon from '@svg/linkedin.svg';
 
 const contactIcons: { [key: string]: JSX.Element } = {
   telegram: telegramIcon(),
   github: githubIcon(),
   behance: behanceIcon(),
   habr: habrIcon(),
+  linkedin: linkedinIcon(),
 };
 
 const getContactIcon = (name: string) => {
